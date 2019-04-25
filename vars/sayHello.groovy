2cde9146499ad12d13040694770171a90ca3f6c1
Dmitry Bogatyr <diboan@yandex.ru>
2019-04-25 17:19:34 +0300

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,16 @@
 /**
-* Hello world step
+* Upload python package
 */
-def call(String name) {
-	echo "Hello $name!"
+def call(String PYPI_USERNAME = 'user', String PYPI_PASS = 'user', String PYPI_REPO = 'localhost') {
+	# just in case if the last command will not execute
+	rm -rfv env dist
+	python3 -m venv env
+	. env/bin/activate
+	pip install twine wheel
+	python setup.py bdist_wheel
+	twine upload  --repository-url  \
+	-u ${PYPI_USERNAME} -p ${PYPI_PASS} dist/* --verbose
+	deactivate
+	rm -rfv env dist
 }
 
