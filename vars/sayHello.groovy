3e7d61e440582f6470da9d24ed511d7259230f30
Dmitry Bogatyr <diboan@yandex.ru>
2019-04-25 17:31:42 +0300

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,11 +3,12 @@
 */
 def call(String PYPI_USERNAME = 'user', String PYPI_PASS = 'user', String PYPI_REPO = 'localhost') {
 	sh '''
-	rm -rfv env dist
-	python3 -m venv env
-	. env/bin/activate
-	pip install twine wheel
-	python setup.py bdist_wheel
+	#rm -rfv env dist
+	#python3 -m venv env
+	#. env/bin/activate
+	#pip install twine wheel
+	#python setup.py bdist_wheel
+	echo ${PYPI_USERNAME} ${PYPI_PASS} ${PYPI_REPO}
 	twine upload  --repository-url ${PYPI_REPO} \
 	-u ${PYPI_USERNAME} -p ${PYPI_PASS} dist/* --verbose
 	deactivate
